bd4888490853e42e986dff320036f76bb99945c7
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2022-08-15 09:28:16 +0200

    removed old tensorSpec file version

--- lib_tensorSpecs (1).py
+++
@@ -1,9 +0,0 @@
-import torch 
-
-# the code is absolutely not optimized for GPU nor could the 
-# 2 site problem fill a GPU thus it might be best to keep it 
-# with device = 'cpu'.
-torchTensorArgs = {
-    "device": torch.device('cpu'),
-    "dtype" : torch.cdouble
-}
