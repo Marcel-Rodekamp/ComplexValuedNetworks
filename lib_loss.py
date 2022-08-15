ba5b2e6706e14134ffe884a7d90c9413b22e663d
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2022-08-15 09:22:59 +0200

    removed old loss file version

--- lib_loss.py
+++ lib_loss.py
@@ -1,23 +1,44 @@
 import torch
 
 class StatisticalPowerLoss(torch.nn.Module):
+    
+    # the loss function that maximises the statistical power
 
     def __init__(self, Hubbard2SiteModel, partitionFunctionEstimate = 1):
+        
+        r"""
+            \param: Hubbard2SiteModel, class
+            \param: partitionFunctionEstimate, int
+        """
         super(StatisticalPowerLoss,self).__init__()
 
         self.HM = Hubbard2SiteModel
 
+        # register the estimate of the partition function 
         self.register_buffer(
             name = 'Z',
             tensor = torch.tensor([partitionFunctionEstimate])
         )
 
     def forward(self, phi, logDetJ_NN):
+        
+        r"""
+            \param: phi = torch.tensor(batchSize, Nt, Nx)
+            \param: logDetJ_NN, torch.tensor(batchSize)
+        """
+        # the effective action is the action minus the log det of the NN
         Seff = self.HM.calculate_batch_action(phi) - logDetJ_NN
+        
+        # the normalised statistical power:
+        # \sigma = 
+        
         return torch.exp(-Seff).abs().mean()/self.Z
 
 
 class MinimizeImaginaryPartLoss(torch.nn.Module):
+    
+    # the loss function that minimises the imaginary part of the difference between the action and the log det of the NN
+    
     def __init__(self, Hubbard2SiteModel):
         super(MinimizeImaginaryPartLoss,self).__init__()
 
@@ -25,5 +46,7 @@ class MinimizeImaginaryPartLoss(torch.nn.Module):
 
     def forward(self,phi,logDetJ_NN):
         Seff = self.HM.calculate_batch_action(phi) - logDetJ_NN
-        return (Seff.imag).abs().mean()
+        with torch.no_grad():
+            error = (Seff.imag).abs().var()
+        return (Seff.imag).abs().mean(), error
 
